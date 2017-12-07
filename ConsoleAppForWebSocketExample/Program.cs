69d3a596b66f5f7b17b9b5d75114e5bc8833c2e6
Ihar_Dubina <Ihar_Dubina@EPBYBREW0301>
2017-12-07 18:08:39 +0300

    test commit

--- ConsoleAppForWebSocketExample/Program.cs
+++ ConsoleAppForWebSocketExample/Program.cs
@@ -18,9 +18,9 @@ namespace ConsoleAppForWebSocketExample
         static void Main(string[] args)
         {
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-             
-            var ws = new ClientWebSocket();
 
+            using (var ws = new ClientWebSocket())
+            { 
             #region sendMessage(Cex.io)
             var subscribe = new Subsribe();
             subscribe.E = "subscribe";
@@ -55,101 +55,45 @@ namespace ConsoleAppForWebSocketExample
              ws.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes(jsonInit)), WebSocketMessageType.Text, true, CancellationToken.None).Wait();
 
            
-            var buffer = new byte[8000];
-             while (true)
-             {
-                 var segment = new ArraySegment<byte>(buffer);
-
-
-                var result =   ws.ReceiveAsync(segment, CancellationToken.None).Result;
-                //ws.ReceiveAsync(segment, CancellationToken.None).Wait();
-                 
-
-                  int count = result.Count;
-                 while (!result.EndOfMessage)
-                 {
-                    if (count >= buffer.Length)
-                    {
-                        break;
-                    }
-                    segment = new ArraySegment<byte>(buffer, count, buffer.Length - count);
-                     result =   ws.ReceiveAsync(segment, CancellationToken.None).Result;
-                     count += result.Count;
-                 } 
-
-                 var message = Encoding.UTF8.GetString(buffer,0,count); //buffer
-
-                TradeData data;
-                if (message.Contains("md") && !message.Contains("md_groupped"))
+            var buffer = new byte[3000];
+                while (true)
                 {
-                    data = JsonConvert.DeserializeObject<TradeData>(message);
-                    Console.Write(data.Data.Buy[0][0]);    // 0-49 0-1
-                    Console.WriteLine(" " + data.Data.Buy[0][1]+" "+data.Data.BuyTotal);
+                    var segment = new ArraySegment<byte>(buffer);
 
+
+                    var result = ws.ReceiveAsync(segment, CancellationToken.None).Result;
+                    //ws.ReceiveAsync(segment, CancellationToken.None).Wait();
+
+
+                    int count = result.Count;
+                    while (!result.EndOfMessage)
+                    {
+                        if (count >= buffer.Length)
+                            break; //skip this message
+
+                        segment = new ArraySegment<byte>(buffer, count, buffer.Length - count); //write in buffer begining from count
+                        result = ws.ReceiveAsync(segment, CancellationToken.None).Result;
+                        count += result.Count;
+                    }
+
+                    var message = Encoding.UTF8.GetString(buffer, 0, count); //buffer
+
+                    TradeData data;
+                    if (message.Contains("md") && !message.Contains("md_groupped"))
+                    {
+                        data = JsonConvert.DeserializeObject<TradeData>(message);
+                        Console.Write(data.Data.Buy[0][0]);    // 0-49 0-1
+                        Console.WriteLine(" " + data.Data.BuyTotal);
+
+                    }
                 }
                   
-                   //string e = data.E;
                     
                   // Console.WriteLine(message);
              } 
 
         }
 
-        #region
-        /* static async Task Client()
-        {
-            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-            var socket = new ClientWebSocket();
-            var uri = new Uri("wss://ws.cex.io/ws");
-
-
-            //socket.Options.SetRequestHeader("Connection", "Upgrade");
-             
-            await socket.ConnectAsync(uri, CancellationToken.None);
-
-            ArraySegment<Byte> buffer = new ArraySegment<byte>(new Byte[5000]);
-            WebSocketReceiveResult result = null;
-
-
-            while (socket.State == WebSocketState.Open)
-            {
-
-                Console.WriteLine("hello");
-
-                using (var ms = new MemoryStream())
-                    {
-                        do
-                        {
-                            result = await socket.ReceiveAsync(buffer, CancellationToken.None);
-                            ms.Write(buffer.Array, buffer.Offset, result.Count);
-                        }
-                        while (!result.EndOfMessage);
-
-                        ms.Seek(0, SeekOrigin.Begin);
-
-                    if (result.MessageType == WebSocketMessageType.Close)
-                    {
-                        await
-                            socket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Close response received",
-                                CancellationToken.None);
-                    }
-
-                    if (result.MessageType == WebSocketMessageType.Text)
-                        {
-                           string jsonResult = "";
-                            using (var reader = new StreamReader(ms, Encoding.UTF8))
-                            {
-                               jsonResult = reader.ReadToEnd();
-                            }
-                        TradeData results = JsonConvert.DeserializeObject<TradeData>(jsonResult);
-                        //Console.WriteLine(results.Data.BuyTotal);
-
-                        }
-                    
-                }
-               // result = await socket.ReceiveAsync(buffer, CancellationToken.None);
-            }
-        }*/
-        #endregion
+         
     }
 }
